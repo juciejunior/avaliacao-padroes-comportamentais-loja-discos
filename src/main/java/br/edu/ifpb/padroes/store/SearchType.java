28cb0fc110dd71dde51051e908a3cd2cf6f020a8
Victor <vbatista.dev@gmail.com>
2025-12-09 15:17:46 -0300

    Refatorando o Enum SearchType

--- src/main/java/br/edu/ifpb/padroes/store/SearchType.java
+++ src/main/java/br/edu/ifpb/padroes/store/SearchType.java
@@ -1,5 +1,39 @@
 package br.edu.ifpb.padroes.store;
 
+import br.edu.ifpb.padroes.music.Album;
+
 public enum SearchType {
-    TITLE, ARTIST, GENRE, TYPE
+    TITLE {
+        @Override
+        public boolean matches(Album album, String term) {
+            return album.getTitle().toLowerCase()
+                    .contains(term.toLowerCase());
+        }
+    },
+
+    ARTIST {
+        @Override
+        public boolean matches(Album album, String term) {
+            return album.getArtist().toLowerCase()
+                    .contains(term.toLowerCase());
+        }
+    },
+
+    GENRE {
+        @Override
+        public boolean matches(Album album, String term) {
+            return album.getGenre().toLowerCase()
+                    .contains(term.toLowerCase());
+        }
+    },
+
+    TYPE {
+        @Override
+        public boolean matches(Album album, String term) {
+            return album.getType().name()
+                    .equalsIgnoreCase(term);
+        }
+    };
+
+    public abstract boolean matches(Album album, String term);
 }
